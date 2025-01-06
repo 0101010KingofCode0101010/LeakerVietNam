7d793cd6f90960c13071e8576792ce1a62a211ac
0101010KingofCode0101010 <140817935+0101010KingofCode0101010@users.noreply.github.com>
2025-01-07 00:52:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    fetch('https://0101010kingofcode0101010.github.io/LeakerVietNam/videos.json')
+    fetch('https://github.com/0101010KingofCode0101010/LeakerVietNam/blob/main/videos.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
